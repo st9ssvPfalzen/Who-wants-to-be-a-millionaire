75cc3717cbc47c8c5a463134850d5a07d672e93c
SusanneTaferner <susianne.taferner@tum.de>
2026-05-16 16:11:45 +0100

    deleted files

--- app.py
+++
@@ -1,8 +0,0 @@
-import json
-
-# Open the questions.json file and load it into Python
-with open("questions.json", "r", encoding="utf-8") as file:
-    questions = json.load(file)
-
-# Print only the first question dictionary
-print(questions[0])
